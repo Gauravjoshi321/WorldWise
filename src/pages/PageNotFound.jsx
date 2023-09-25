dc613b03700f5cca946316e485f1a3e1b688cbe9
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-09-25 18:38:20 +0530

    newFiles

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import NavBar from "../components/PageNav";
-
-function PageNotFound() {
-  return <div>
-    <NavBar/>
-    PageNotFound
-  </div>
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound;
