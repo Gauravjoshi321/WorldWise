19f0403210d3a74568c8f106cd0f1990ce7f29c0
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-09-26 20:22:55 +0530

    latlng

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,15 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   return (
     <div className={styles.mapContainer}>
-      Map
+      {lat} {lng}
     </div>
   )
 }
