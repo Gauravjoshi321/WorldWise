a51e1654bf8c5981bce0f2e9fdc3411a9f649cc2
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-10-04 18:22:15 +0530

    unauth

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,15 +2,25 @@ import SideBar from "../components/SideBar";
 import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
 import User from "../components/User";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function AppLayout() {
-  return (
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  useEffect(function () {
+    if (!isAuthenticated) navigate('/')
+  }, [isAuthenticated, navigate])
+
+  return isAuthenticated ? (
     <div className={styles.app}>
       <SideBar />
       <Map />
       <User />
     </div>
-  )
+  ) : null
 }
 
 export default AppLayout;
