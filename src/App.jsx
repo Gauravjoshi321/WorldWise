c549d5b669f01c2e5e739a2ae81bd5a568423d01
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-10-09 18:59:04 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-// import { useEffect, useState } from "react";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import HomePage from './pages/HomePage';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
@@ -10,8 +12,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
+
 
 function App() {
 
