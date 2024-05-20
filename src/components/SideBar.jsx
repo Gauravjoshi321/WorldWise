b236a54d197035196680974c2f444a64e54d4013
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-05-20 20:42:46 +0530

    new added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,22 +1,22 @@
 import Logo from "./Logo";
-import styles from "./SideBar.module.css";
+import styles from "./Sidebar.module.css";
 import AppNav from "../components/AppNav";
 import { Outlet } from "react-router-dom";
 
 
-function SideBar () {
+function SideBar() {
   return (
     <div className={styles.sidebar}>
-     <Logo/>
-     <AppNav/>
+      <Logo />
+      <AppNav />
 
-     <Outlet />
+      <Outlet />
 
-     <footer className={styles.footer}>
-      <p className={styles.copyright}>
-        &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-      </p>
-     </footer>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
     </div>
   )
 }
